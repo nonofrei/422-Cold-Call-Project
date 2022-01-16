ac1635801aed723a9264f9b4e579e4283828e30a
hzhang9 <82175945+hzhang9@users.noreply.github.com>
2022-01-15 22:31:06 -0800

    Add initial interface and file operation interface(termial version) into view
    And main.py now call firstInterface() to get start, the file operation interface now use option 1 version(termial version).
    At the top of viewAndController.py can change key link.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     restList=['name_1', 'name_2', 'name_3', 'name_4','name_5','name_6','name_7','name_8','name_9']
     
     interface=UserInterface(studentList,restList)
-    interface.CCinterface()
+    interface.firstInterface()
     #!!!interface.CCinterface() must be the last step of main(), because before the cold call windows close, not code run behind the line self.app.mainloop() in func CCinterface()
 
 if __name__ == '__main__':
--- viewAndController.py
+++ viewAndController.py
@@ -1,20 +1,21 @@
 import tkinter as tk
 import random
 
+remove1='1'
+remove2='2'
+remove3='3'
+remove4='4'
+flag1='q'
+flag2='w'
+flag3='e'
+flag4='r'
 
 class UserInterface(object):
 
     def __init__(self, nameList,restList):
-        self.app = tk.Tk()
         self.nameList=nameList
         self.restList=restList
-        #self.fileMode=False
-        tempList=[]
-        for i in nameList:
-            tempName=tk.StringVar()
-            tempName.set(i)
-            tempList.append(tempName)
-        self.tempList=tempList
+        self.fileMode=False
         
     def random_select(self):
         #need ramdom algorithm here
@@ -24,7 +25,6 @@ class UserInterface(object):
     def removeAndAddStudent_1(self,event):
         #need database(model) part, save remove information
         #need insert the removed name back to restList
-        print('place 1')
         removedName=self.nameList[0]
         self.nameList[0]=self.nameList[1]
         self.nameList[1]=self.nameList[2]
@@ -42,7 +42,6 @@ class UserInterface(object):
     def removeAndAddStudent_2(self,event):
         #need database(model) part, save remove information
         #need insert the removed name back to restList
-        print('place 2')
         removedName=self.nameList[1]
         self.nameList[1]=self.nameList[2]
         self.nameList[2]=self.nameList[3]
@@ -58,7 +57,6 @@ class UserInterface(object):
     def removeAndAddStudent_3(self,event):
         #need database(model) part, save remove information
         #need insert the removed name back to restList
-        print('place 3')
         removedName=self.nameList[2]
         self.nameList[2]=self.nameList[3]
         random_name=self.random_select()
@@ -72,7 +70,6 @@ class UserInterface(object):
     def removeAndAddStudent_4(self,event):
         #need database(model) part, save remove information
         #need insert the removed name back to restList
-        print('place 4')
         removedName=self.nameList[3]
         random_name=self.random_select()
         self.nameList[3]=random_name
@@ -85,70 +82,82 @@ class UserInterface(object):
     # qwer both are flag, so 1 func is enough?
     def flag(self,event):
         print("flag!")
-    
-    '''#test Terminal display
-    def closeWin(self,event):
-        print('place 5')
-        self.fileMode=True
-        self.app.destroy()'''
 
+    #initial view
+    def firstInterface(self):
+        root=tk.Tk()
+        root.wm_attributes('-topmost',1)
+        root.title('Model select')
+        button_1=tk.Button(root,text='Cold Call Assist',command=lambda:[root.destroy(),self.CCinterface()])
+        button_1.grid(row=0,column=0)
+        button_2=tk.Button(root,text='Input or output file',command=lambda:[root.destroy(),self.fileInterface()])
+        button_2.grid(row=1,column=0)
+        root.mainloop()
+        
+    #tkinter window have been closed, now in termial
+    def fileInterface(self):
+        while True:
+            command=input('Input or output file: ')
+            commandList=command.split()
+            if len(commandList)==2:
+                if commandList[0]=='input' or commandList[0]=='output':
+                    if commandList[0]=='input':
+                        print('Input file',commandList[1])
+                    else:
+                        print('Output file',commandList[1])
+                else:
+                    print('Invalid command')
+            elif len(commandList)==1:
+                if commandList[0]=='quit':
+                    break
+                else:
+                    print('Invalid command')
+
+    #cold call view
     def CCinterface(self):
+        self.app = tk.Tk()
+        
+        tempList=[]
+        for i in self.nameList:
+            tempName=tk.StringVar()
+            tempName.set(i)
+            tempList.append(tempName)
+        self.tempList=tempList
+        
         self.app.wm_attributes('-topmost',1)
         self.app.title('Cold-Call Assist')
         tk.Label(self.app, text='Next students:').pack(side='left')
 
         theLabel_1 = tk.Label(self.app, textvariable=self.tempList[0])
         theLabel_1.pack(side='left')
-        self.app.bind("1",self.removeAndAddStudent_1)
+        self.app.bind(remove1,self.removeAndAddStudent_1)
 
         tk.Label(self.app, text=' ').pack(side='left')
 		
         theLabel_2 = tk.Label(self.app, textvariable=self.tempList[1])
         theLabel_2.pack(side='left')
-        self.app.bind("2",self.removeAndAddStudent_2)
+        self.app.bind(remove2,self.removeAndAddStudent_2)
 
         tk.Label(self.app, text=' ').pack(side='left')
 
         theLabel_3 = tk.Label(self.app, textvariable=self.tempList[2])
         theLabel_3.pack(side='left')
-        self.app.bind("3",self.removeAndAddStudent_3)
+        self.app.bind(remove3,self.removeAndAddStudent_3)
 
         tk.Label(self.app, text=' ').pack(side='left')
 
         theLabel_4 = tk.Label(self.app, textvariable=self.tempList[3])
         theLabel_4.pack(side='left')
-        self.app.bind("4",self.removeAndAddStudent_4)
+        self.app.bind(remove4,self.removeAndAddStudent_4)
         
-        self.app.bind("q",self.flag)
-        self.app.bind("w",self.flag)
-        self.app.bind("e",self.flag)
-        self.app.bind("r",self.flag)
-        #self.app.bind("5",self.closeWin)
-
-        print('done1')
+        self.app.bind(flag1,self.flag)
+        self.app.bind(flag2,self.flag)
+        self.app.bind(flag3,self.flag)
+        self.app.bind(flag4,self.flag)
 		
         self.app.mainloop()
         
-        '''#test Terminal display
-        if self.fileMode==True:
-            while True:
-                command=input('Input or output file: ')
-                commandList=command.split()
-                if len(commandList)==2:
-                    if commandList[0]=='input' or commandList[0]=='output':
-                        if commandList[0]=='input':
-                            print('Input file',commandList[1])
-                        else:
-                            print('Output file',commandList[1])
-                    else:
-                        print('Invalid command')
-                elif len(commandList)==1:
-                    if commandList[0]=='quit':
-                        break
-                    else:
-                        print('Invalid command')
-        '''
-        print('done2')
+        print('done')
 
 
 
