4dab1e4ec1dd2dedaa73e158d2d208fcc1ddbf0a
nonofrei <84356514+nonofrei@users.noreply.github.com>
2022-01-30 18:30:40 -0800

    Organized Everything

---
+++ 422CCP-main/Python Files/ColdCallOperation.py
@@ -0,0 +1,337 @@
+import tkinter as tk
+import random
+from time import *
+from datetime import datetime
+from threading import Thread
+import os
+
+import Testing
+from ImportAndExport import *
+from collections import *
+from queue import *
+from Testing import *
+
+remove1 = '1'
+remove2 = '2'
+remove3 = '3'
+remove4 = '4'
+
+flag1 = 'q'
+flag2 = 'w'
+flag3 = 'e'
+flag4 = 'r'
+
+test_key = 't'
+
+
+class UserInterface(object):
+
+    def __init__(self):
+        self.nameList = []
+        self.restList = []
+
+        self.studentQueue = PriorityQueue()  # randomized priority queue
+        self.infoMap = dict()               # hash map for storing student info
+        self.classSize = 0                  # for storing number of students
+        self.removedName = None
+
+        # for 1 sec flagging and avoiding double flagging
+        self.keypress_timer = time()
+        self.can_flag = True
+        self.test_flag = False
+
+    def print_summary_line(self):
+        sleep(1)
+        studentInfo = self.infoMap[self.removedName.name]
+        output = studentInfo[0] + "\t" + studentInfo[1] + "\t<" + studentInfo[3] + ">\n"
+        if not self.can_flag:
+            output = "X\t" + output
+        self.summaryFile.write(output)
+
+    def removeAndAddStudent_1(self, event):
+
+        # 1 sec delay and allow flagging
+        self.keypress_timer = time()
+        self.can_flag = True
+
+        # remove corresponding student and increase their call counter
+        # add them back to the priority queue
+        self.removedName = self.nameList[0]
+        self.removedName.priority += 1
+        self.studentQueue.put(self.removedName)
+
+        # slide students to the left on the display
+        self.nameList[0] = self.nameList[1]
+        self.nameList[1] = self.nameList[2]
+        self.nameList[2] = self.nameList[3]
+
+        # get next student from the priority queue
+        nextStudent = self.studentQueue.get()
+        self.nameList[3] = nextStudent
+
+        # notify display to reflect the changes in the display names
+        self.tempList[0].set(self.nameList[0].name)
+        self.tempList[1].set(self.nameList[1].name)
+        self.tempList[2].set(self.nameList[2].name)
+        self.tempList[3].set(nextStudent.name)
+
+        # print to output file, use a separate thread so that our main thread
+        # is not waiting on if the flagging occurs
+        thread = Thread(target=self.print_summary_line)
+        thread.start()
+
+    def removeAndAddStudent_2(self, event):
+
+        # 1 sec delay and allow flagging
+        self.keypress_timer = time()
+        self.can_flag = True
+
+        # remove corresponding student and increase their call counter
+        # add them back to the priority queue
+        self.removedName = self.nameList[1]
+        self.removedName.priority += 1
+        self.studentQueue.put(self.removedName)
+
+        # slide names to the left on the display
+        self.nameList[1] = self.nameList[2]
+        self.nameList[2] = self.nameList[3]
+
+        # get next student from the priority queue
+        nextStudent = self.studentQueue.get()
+        self.nameList[3] = nextStudent
+
+        # notify display to reflect the changes in the display names
+        self.tempList[1].set(self.nameList[1].name)
+        self.tempList[2].set(self.nameList[2].name)
+        self.tempList[3].set(nextStudent.name)
+
+        # print to output file, use a separate thread so that our main thread
+        # is not waiting on if the flagging occurs
+        thread = Thread(target=self.print_summary_line)
+        thread.start()
+
+    def removeAndAddStudent_3(self, event):
+
+        # 1 sec delay and allow flagging
+        self.keypress_timer = time()
+        self.can_flag = True
+
+        # remove corresponding student and increase their call counter
+        # add them back to the priority queue
+        self.removedName = self.nameList[2]
+        self.removedName.priority += 1
+        self.studentQueue.put(self.removedName)
+
+        # slide names to the left on the display
+        self.nameList[2] = self.nameList[3]
+
+        # get next student from the priority queue
+        nextStudent = self.studentQueue.get()
+        self.nameList[3] = nextStudent
+
+        # notify display to reflect the changes in the display names
+        self.tempList[2].set(self.nameList[2].name)
+        self.tempList[3].set(nextStudent.name)
+
+        # print to output file, use a separate thread so that our main thread
+        # is not waiting on if the flagging occurs
+        thread = Thread(target=self.print_summary_line)
+        thread.start()
+
+    def removeAndAddStudent_4(self, event):
+
+        # 1 sec delay and allow flagging
+        self.keypress_timer = time()
+        self.can_flag = True
+
+        # remove corresponding student and increase their call counter
+        # add them back to the priority queue
+        self.removedName = self.nameList[3]
+        self.removedName.priority += 1
+        self.studentQueue.put(self.removedName)
+
+        # get next student from priority queue
+        nextStudent = self.studentQueue.get()
+        self.nameList[3] = nextStudent
+
+        # notify display to reflect the changes in the display names
+        self.tempList[3].set(nextStudent.name)
+
+        # print to output file, use a separate thread so that our main thread
+        # is not waiting on if the flagging occurs
+        thread = Thread(target=self.print_summary_line)
+        thread.start()
+
+    def flag(self, event):
+        time_difference = time() - self.keypress_timer
+
+        # within 1 sec and can flag
+        if time_difference <= 1 and self.can_flag:
+            self.can_flag = False
+
+    def test_flagging(self, event):
+        # test_flagging checks to see if the 't' key has already been pressed once
+        # If it has then it calls initiate_testing
+        # If not then it sets the test_flag to True
+        if self.test_flag == False:
+            self.test_flag = True
+            return
+
+        if self.test_flag:
+            self.initiate_testing()
+            self.test_flag = False
+
+    def initiate_testing(self):
+        # This method calls on test_warningInterface to initiate the testing procedure
+        if self.test_flag:
+            Testing.testwarningInterface()
+        self.test_flag = False
+
+    def inputFile(self):
+        if os.path.exists('CurrentRoster.txt') == False:
+            ImportFile()
+        else:
+            self.warningInterface()
+
+    def confirmInput(self):
+        ImportFile()
+
+    def outputFile(self):
+        ExportFile()
+
+    # initial view
+    def firstInterface(self):
+        self.app = tk.Tk()
+        self.app.geometry('250x90+0+0')
+        self.app.wm_attributes('-topmost', 1)
+        self.app.title('Model select')
+        button_1 = tk.Button(self.app, text='Cold Call Assist', fg="black", bg="white", command=lambda: [self.CCinterface()])
+        button_1.pack()
+
+        button_2 = tk.Button(self.app, text='Import New File', fg="black", bg="white", command=lambda: self.inputFile())
+        button_2.pack()
+
+        button_3 = tk.Button(self.app, text='Select Location to Export Student File', fg="black", bg="white", command=lambda: self.outputFile())
+        button_3.pack()
+
+        self.app.mainloop()
+
+    def warningInterface(self):
+        root = tk.Tk()
+        root.geometry('+0+0')
+        root.wm_attributes('-topmost', 1)
+        root.title('Warning')
+        theLabel = tk.Label(root, fg='red', text='Warning! Preexisting student data, do you want to overwrite it?')
+        theLabel.pack()
+        button_1 = tk.Button(root, text='Continue',
+                             command=lambda: [root.destroy(), self.confirmInput()])
+        button_1.pack()
+        button_2 = tk.Button(root, text='Back', command=lambda: [root.destroy()])
+        button_2.pack()
+
+    # cold call view
+    def CCinterface(self):
+        # check if there is a saved file with student info
+        try:
+            currentRoster = open("CurrentRoster.txt", "r")
+        except:
+            FileError("No Existing Student List", "Please Import Student List")
+            return
+
+        # process student list file
+        self.restList = deque(ScanFile(currentRoster, True))
+
+        # check that it read the file correctly
+        if self.restList.popleft() == "E":
+            FileError("Error Reading Current Student List", "Please Import New Student List")
+            return
+
+        # create random queue and hash map with information
+        self.classSize = len(self.restList)
+        for i in range(self.classSize):
+            student = random.choice(self.restList)
+            name = student[0] + " " + student[1]
+            self.studentQueue.put(Student(name, 0))
+            self.infoMap[name] = student
+            self.restList.remove(student)
+        # add 4 students to the display list
+        for i in range(4):
+            self.nameList.append(self.studentQueue.get())
+
+        # create summary file to write to
+        now = datetime.now()
+        fileName = now.strftime("%b-%d-%Y_%H:%M:%S") + "_Summary.txt"
+        self.summaryFile = open(fileName, "w")
+
+        # we can now safely close the main menu, open cold call assist
+        self.app.destroy()
+
+        # create window
+        root = tk.Tk()
+        root.geometry('+0+0')
+
+        tempList = []
+        for i in self.nameList:
+            tempName = tk.StringVar()
+            tempName.set(i.name)
+            tempList.append(tempName)
+        self.tempList = tempList
+
+        root.wm_attributes('-topmost', 1)
+        root.title('Cold-Call Assist')
+        tk.Label(root, text='Next students:').grid(row=0, column=0)
+
+        theLabel_1 = tk.Label(root, textvariable=self.tempList[0])
+        theLabel_1.grid(row=0, column=1)
+        root.bind(remove1, self.removeAndAddStudent_1)
+
+        tk.Label(root, text=' ').grid(row=0, column=2)
+
+        theLabel_2 = tk.Label(root, textvariable=self.tempList[1])
+        theLabel_2.grid(row=0, column=3)
+        root.bind(remove2, self.removeAndAddStudent_2)
+
+        tk.Label(root, text=' ').grid(row=0, column=4)
+
+        theLabel_3 = tk.Label(root, textvariable=self.tempList[2])
+        theLabel_3.grid(row=0, column=5)
+        root.bind(remove3, self.removeAndAddStudent_3)
+
+        tk.Label(root, text=' ').grid(row=0, column=6)
+
+        theLabel_4 = tk.Label(root, textvariable=self.tempList[3])
+        theLabel_4.grid(row=0, column=7)
+        root.bind(remove4, self.removeAndAddStudent_4)
+
+        buttonBack = tk.Button(root, text='Back', fg="black", bg="white", command=lambda: [root.destroy(), self.firstInterface()])
+        buttonBack.grid(row=1, sticky='w')
+
+        root.bind(flag1, self.flag)
+        root.bind(flag2, self.flag)
+        root.bind(flag3, self.flag)
+        root.bind(flag4, self.flag)
+
+        # Bind the test key to the appropiate method
+        root.bind(test_key, self.test_flagging)
+
+        root.mainloop()
+
+
+class Student():
+
+    def __init__(self, name, priority):
+        self.name = name
+        self.priority = priority
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+
+# initiate cold call system
+def main():
+    interface = UserInterface()
+    interface.firstInterface()
+
+
+if __name__ == '__main__':
+    main()
---
+++ 422CCP-main/Python Files/ImportAndExport.py
@@ -0,0 +1,183 @@
+"""
+	This file contains the functionality needed in order to work with files. The file name implies only import however there are multiple functions defined.
+
+	Authors:
+		Zacree Carroll: ImportFile(), ExportFile(), ScanFile()
+		Luke Vandecasteele: FileError()
+	
+	Created:
+		1/16/22
+
+	Last Modified:
+		1/24/22
+"""
+from tkinter import filedialog, Text, Label
+import tkinter as tk
+
+"""
+    This function provides the functionality for the import file button. It opens a file searching window and allows
+    the user to search their computer for their desired file. They are only allowed to select .txt files within this 
+    window that pops up. Once the file has been selected the specified format is checked for and if the format is
+    incorrect then the function will return information about the error encountered to the caller 
+    (see return value) below.
+                                --------------------------------------------
+
+        filePath: Varaible which contains the path name for the file selected from the pop up window, uses tkinter 
+                  library
+         
+         scanRes: Variable containing the result of the format checking
+         
+         newFile: File pointer for the copied roster
+         
+    return value: Returns a tuple (<Line Number>, <Error string>), if the tuple (0,0) is returned then the action
+                  was successful and the file has been successfully imported
+"""
+
+
+def ImportFile():
+    # Ask user to browse for their file
+    # use tkinter library to avoid OS specific approach to open a file selection window and save the path of the file
+    filePath = filedialog.askopenfilename(initialdir="/Users/", title="Select Import File",
+                                          filetypes=(("text", "*.txt"), ("all files", "*.*")))
+
+    # Try to open file safely
+    try:
+
+        f = open(filePath, "r")
+
+    # Problem on OS side return error string
+    except OSError as err:
+
+        return "Unable to open file:", err.strerror
+
+    scanRes = ScanFile(f, False)
+
+    if scanRes[0] != 0:
+        FileError(scanRes[1], 'Improper File Format. Please fix error indicated in window title.', ImportFile)
+
+    newFile = open("CurrentRoster.txt", "w")
+
+    f.seek(0)  # Reset file pointer to beginning of file
+
+    # Copy file to working directory
+    for line in f.readlines():
+        newFile.write(line)
+
+    f.close()
+    newFile.close()
+
+
+"""
+    This function will return values based on the value of returnStudentList. If this value is True then ScanFile will
+    return a list. Contained in this list will be lists of individual student information. The format of the return 
+    value is ["E", <Error Message>] or ["L", [<First Name>, <Last Name>...] .... [<First Name>, <Last Name>...]].
+    When using this function before proceeding with return values you need to check what the first value in the list is
+    and act accordingly.
+"""
+
+
+def ScanFile(f, returnStudentList):
+    errorDict = {
+        -1: "First or last name contained invalid character",
+        -2: "Problem with UO ID",
+        -3: "Problem with email address",
+        -4: "Reveal code contains non-numeric characters",
+        -7: "Contained invalid white-space character",
+        -8: "Line is missing information"
+    }
+
+    studentInfoLists = []
+    studentInfoLists.append("L")
+
+    for lnum, line in enumerate(f.readlines()):
+
+        student = []
+        splitLine = line.split("\t")
+
+        # analyze each individual line in the file for correctness
+        for cnum, comp in enumerate(splitLine):
+
+            """
+            here we will return a tuple containing the line number the error was found on
+            and the component which contained the issue
+            """
+            # check for proper amount of components
+            if (len(splitLine) < 5):
+                return ["E", f"Error on line {lnum}: {errorDict[-8]}"]
+
+            # if space is found then file tabbing was done in correctly
+            if comp.find(" ") > -1:
+                return ["E", f"Error on line {lnum}: {errorDict[-7]}"]
+
+            # check first and last name with same method
+            elif cnum == 0 or cnum == 1:
+                if not comp.isalpha():
+                    return ["E", f"Error on line {lnum}: {errorDict[-1]}"]
+
+            # check UO ID must be a number 9 digits long and start with 951
+            elif cnum == 2:
+                if not comp.isdigit() or len(comp) != 9 or comp.find("951", 0, 3) == -1:
+                    return ["E", f"Error on line {lnum}: {errorDict[-2]}"]
+
+            # check email for @ and . not very robust check but works for now
+            # TODO: Possibly add regex check for email validity
+            elif cnum == 3:
+                if comp.find("@") == -1 or comp.find(".") == -1:
+                    return ["E", f"Error on line {lnum}: {errorDict[-3]}"]
+
+            # TODO: Possibly add regex check for phonetic spelling validity here
+
+            # file may not contain phonetic spellings so find out what the 5th component is
+            elif cnum >= 4:
+                if len(splitLine) == 5:  # if true then this is a reveal code
+                    if not comp.isdigit():
+                        return ["E", f"Error on line {lnum}: {errorDict[-4]}"]
+
+            student.append(comp)
+
+        studentInfoLists.append(student)
+
+    if returnStudentList:
+        return studentInfoLists
+
+    else:
+        return (0, 0)
+
+
+def ExportFile():
+    dirPath = filedialog.askdirectory()
+    copyFile = ""
+    currentRoster = ""
+
+    if dirPath == '':
+        return
+
+    else:
+        # open new file for copying
+        try:
+
+            currentRoster = open("CurrentRoster.txt", "r")
+
+        # otherwise handle exception and call FileError
+        except:
+            FileError("No current roster found", "Please import a roster file before attempting to export")
+            return
+        else:
+            dirPath = dirPath + "/Cold_Call_Roster.txt"
+            copyFile = open(dirPath, "w")
+            for line in currentRoster.readlines():
+                copyFile.write(line)
+
+    copyFile.close()
+    currentRoster.close()
+
+def FileError(error, buttonMessage, func=None):
+    message_window = tk.Tk()
+    message_window.geometry("700x50+500+700")
+    message_window.title(f"Error: {error}")
+    if func != None:
+        back_button = tk.Button(message_window, text=buttonMessage, command=lambda: [message_window.destroy(), func])
+    else:
+        back_button = tk.Button(message_window, text=buttonMessage, command=message_window.destroy)
+    back_button.place(x=125, y=15)
+    message_window.mainloop()
